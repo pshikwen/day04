8244fd2ca3637f8f032a6d2fca374332dc249554
Pfumelo Shikweni <pshikwen@c4r13s8.wethinkcode.co.za>
2020-06-25 15:04:13 +0200

    done

---
+++ ex03/ft_recursive_power.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	else if (power == 1)
+		return (nb);
+	else
+		return (nb * ft_recursive_power(nb, power - 1));
+}
+
+int	ft_recursive_power(int nb, int power);
+
+int main(){
+	int n = ft_recursive_power(2, -2);
+	printf("%i\n", n);
+}
